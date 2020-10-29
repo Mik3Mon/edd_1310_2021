8f9c479524aff36e1a4586e3c262a1af2e1acb70
Mik3Mon <mmonzonjr@gmail.com>
2020-10-29 13:04:19 -0600

    29octubre_1310

---
+++ ADTS/pruebas.py
@@ -0,0 +1,15 @@
+from arrays import Array
+
+algo = Array(10)
+print(algo.get_item(6363))
+algo.set_item(555,3)
+print(algo.get_item(3))
+print(f"El arreglo tiene {algo.get_length()} elementos")
+algo.clear(777)
+print(algo.get_item(3))
+print("------------------")
+print("Aqui empieza mi prueba del iterador")
+for x in algo:
+    print(x)
+for x in range(algo.get_length()):
+    print(f"{x}->{algo.get_item(x)}")
