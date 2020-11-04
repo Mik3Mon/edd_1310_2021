511fa10d3eb80e42faf28026b0a9a6d10d671236
Mik3Mon <mmonzonjr@gmail.com>
2020-11-04 15:56:21 -0600

    Tarea 4

--- Tarea_4/Main.py
+++ Tarea_4/Main.py
@@ -1,28 +1,31 @@
 from Arrays import Array
 
-arch = open("junio.txt","rt")
+arch = open("junio.txt","r")
 Empleado = arch.readlines()
 Empleado = [ e.replace('','').strip().split(',') for e in Empleado ]
 lista = len(Empleado)
 Empleados = Array(lista)
 extra = float(276.5)
 
-for x in range(lista):
+for x in range(1,lista):
     Empleados.set_item(Empleado[x],x)
 
-for x in range(lista):
+print("EMPLEADOS")
+for x in range(1,lista):
     hora_extra = int(Empleados.get_item(x)[4])
     año_ingreso = int(Empleados.get_item(x)[6])
     sueldo = int(Empleados.get_item(x)[5])
-    sueldo_extra = hora_extra * extra
-    sueldo_total = sueldo + sueldo_extra
-    sueldo_prestacion = float((((2020 - año_ingreso)*0.03)*sueldo)+sueldo_total)
-    print(f" N. empleado:{Empleado[x][0]} Nombre:{Empleado[x][1]} {Empleado[x][2]} {Empleado[x][3]} tiene un sueldo de {sueldo_prestacion}")
+    sueldo_extra = sueldo + (hora_extra * extra)
+    sueldo_prestacion = (2020 - año_ingreso) * 0.03
+    sueldo_total = float((sueldo*sueldo_prestacion)+sueldo_extra)
+    print(f" N. empleado:{Empleado[x][0]} Nombre:{Empleado[x][1]} {Empleado[x][2]} {Empleado[x][3]} tiene un sueldo de ${sueldo_total}")
 
-for i in range(lista):
-        if int(Empleados.get_item(i)[6]) == 2016:
-            print(Empleados.get_item(i))
+print("<---------------->")
+for x in range(1,lista):
+        if int(Empleados.get_item(x)[6]) == 2016:
+            print(f"Empleado mas antiguo: {Empleado[x][1]} {Empleado[x][2]} {Empleado[x][3]}, Año de ingreso:{Empleado[x][6]}")
 
-for x in range(lista):
+print("<---------------->")
+for x in range(1,lista):
         if int(Empleados.get_item(x)[6]) == 2020:
-            print(Empleados.get_item(x))
+            print(f"Empleado menos antiguo: {Empleado[x][1]} {Empleado[x][2]} {Empleado[x][3]}, Año de ingreso:{Empleado[x][6]}")
