988be37d74162630bf045d48e5c9e08e8551874b
Mik3Mon <mmonzonjr@gmail.com>
2020-11-04 15:00:24 -0600

    Tarea 4

--- Tarea_4/Main.py
+++ Tarea_4/Main.py
@@ -4,32 +4,25 @@ arch = open("junio.txt","rt")
 Empleado = arch.readlines()
 Empleado = [ e.replace('','').strip().split(',') for e in Empleado ]
 lista = len(Empleado)
-Empleados = Array(leer)
+Empleados = Array(lista)
 extra = float(276.5)
 
-for x in range(0,lista,1):
+for x in range(lista):
     Empleados.set_item(Empleado[x],x)
-    x = x+1
 
-for x in Empleados:
-    print(x)
+for x in range(lista):
+    hora_extra = int(Empleados.get_item(x)[4])
+    año_ingreso = int(Empleados.get_item(x)[6])
+    sueldo = int(Empleados.get_item(x)[5])
+    sueldo_extra = hora_extra * extra
+    sueldo_total = sueldo + sueldo_extra
+    sueldo_prestacion = float((((2020 - año_ingreso)*0.03)*sueldo)+sueldo_total)
+    print(f" N. empleado:{Empleado[x][0]} Nombre:{Empleado[x][1]} {Empleado[x][2]} {Empleado[x][3]} tiene un sueldo de {sueldo_prestacion}")
 
-for y in range(0,lista,1):
-    ml = int(Empleados.get_item(y)[4])
-    ant = int(Empleados.get_item(y)[6])
-    sl = int(Empleados.get_item(y)[5])
-    lm = ml*extra
-    to = sl+lm
-    tna = float((((2020-ant)*0.03)*sl)+to)
-    print(f"{Empleado[y][1]} tiene un sueldo de {tna}")
-    y = y+1
-
-for i in range(0,lista,1):
+for i in range(lista):
         if int(Empleados.get_item(i)[6]) == 2016:
             print(Empleados.get_item(i))
-        i = i+1
 
-for x in range(0,lista,1):
+for x in range(lista):
         if int(Empleados.get_item(x)[6]) == 2020:
             print(Empleados.get_item(x))
-        x = x+1
