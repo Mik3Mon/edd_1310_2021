458933b4fb2cd6c6152f01cd8fd2e5b45669806d
Mik3Mon <mmonzonjr@gmail.com>
2020-11-03 22:32:22 -0600

    Tarea 4

--- Tarea_4/Main.py
+++ Tarea_4/Main.py
@@ -3,13 +3,7 @@ from Arrays import Array
 
 arch = open("junio.txt","rt")
 dat = 'ddd'
+
 while(dat != ('')):
-    dat = arch.readline().strip()
-
-
-
-"""for j in Empleados:
-    print(j)
-for i in range(Empleados.get_length()):
-    print(f"{i}->{Empleados.get_item(i)}")
-"""
+    dat = arch.readline()
+    dat_2 = dat.strip().split(',')
