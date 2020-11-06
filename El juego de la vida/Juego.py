ea6c6bc1c17fcbdb435228bc5a872419bf6afd79
Mik3Mon <mmonzonjr@gmail.com>
2020-11-05 19:03:46 -0600

    5noviembre_1310

---
+++ El juego de la vida/Juego.py
@@ -0,0 +1,28 @@
+from array2D import Array2D
+
+class Juego:
+    CELULA_VIVA = 1
+    CELULA_MUERTA = 0
+
+    def __init__(self , rens , cols , generaciones , poblacion):
+        self.largo = cols
+        self.alto = rens
+        self.grid = Array2D(self.alto , self.largo , 0)
+        self.generaciones = generaciones
+
+        for celula in poblacion:
+            self.grid.set_item(celula[0] , celula[1] , self.CELULA_VIVA)
+
+    def configura_generacion(self , nueva_poblacion):
+        self.grid.clearing()
+        for celula in nueva_poblacion:
+            self.grid.set_item(celula[0] , celula[1] , self.CELULA_VIVA)
+
+    def imprime_grid(self):
+        for r in range(self.grid.get_num_rows()):
+            for c in range(self.grid.get_num_cols()):
+                if self.grid.get_item(r , c) == 0:
+                    print("░░",end = "")
+                else:
+                    print("▓▓",end="")
+            print("")
