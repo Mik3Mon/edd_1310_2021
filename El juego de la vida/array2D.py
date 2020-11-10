1e3f3f3de6b57249ab517fd8a654e2ee3888627c
Mik3Mon <mmonzonjr@gmail.com>
2020-11-10 11:11:33 -0600

    Juego de la vida

--- El juego de la vida/array2D.py
+++ El juego de la vida/array2D.py
@@ -18,13 +18,50 @@ class Array2D:
     def get_num_cols(self):
         return self.__cols
 
-    def get_item(self , row , col):
+    def get_item(self,row,col):
         return self.__array[row][col]
 
-    def set_item(self , row , col , valor):
-        self.__array[row][col]=valor
+    def set_item( self , row , col , valor ):
+        self.__array[row][col] = valor
 
-    def clearing(self , valor = 0):
+    def clearing(self, valor=0):
         for ren in range(self.__rows):
             for col in range(self.__cols):
                 self.__array[ren][col]=valor
+
+# Metodos
+#Método Generación
+
+ def configura_generacion(self,nueva_poblacion):
+        self.grid.clearing()
+        for celula in nueva_poblacion:
+            self.grid.set_item(celula[0],celula[1],self.CELULA_VIVA)
+
+#Método Grid, colorea la matriz
+
+    def imprime_grid(self):
+        for r in range(self.grid.get_num_rows()):
+            for c in range(self.grid.get_num_cols()):
+                if self.grid.get_item(r,c) == 0:
+                    print("░░",end="")
+                else:
+                    print("▓▓",end="")
+            print("")
+
+#Meodo para saber y limitar el número de vecinos
+
+def get_numero_vecinos_vivos(self , row , col ):
+        limites = [ row-1 , row+1 , col-1 , col+1 ]
+        vivos = 0
+        limites = self.__ajusta_limites__(limites)
+        if row >= 0 and row <= self.alto-1 and col >= 0 and col <= self.largo -1:
+            for r in range(limites[0],limites[1]+1):
+                for c in range(limites[2],limites[3]+1):
+                    if r == row and c == col:
+                        continue
+                    else :
+                        if self.grid.get_item(r,c) == self.CELULA_VIVA :
+                            vivos += 1
+        else :
+            print("Coordenada la celula fuera del grid")
+        return vivos
