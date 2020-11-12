14c2155c006b0fc40ac3e156cd46999397703e75
Mik3Mon <mmonzonjr@gmail.com>
2020-11-12 12:45:15 -0600

    12noviembre_1310

--- 12noviembre_1310.py
+++ 12noviembre_1310.py
@@ -31,8 +31,36 @@ tmp = a.siguiente.siguiente.siguiente
 a.siguiente.siguiente.siguiente = Nodo(48)
 a.siguiente.siguiente.siguiente.siguiente = tmp
 
-#Recorrido Transversal
+#Recorrido Transversal a
 curr_node = a
+print(curr_node.dato , "---> " , end = "")
+while(curr_node.siguiente != None):
+    curr_node = curr_node.siguiente
+    print(curr_node.dato , "---> " , end = "")
+print("")
+
+print("--------------------------------------")
+print("\nAgregar Nodos sin poner .siguiente")
+print("")
+class Nodo2:
+    def __init__(self , dato  , sig=None):
+        self.dato = dato
+        self.siguiente = sig
+
+#Ejemplo 1
+b = Nodo2( 10 )
+
+#Ejemplo 2   RECONSTRUCCION
+b = Nodo2(10 , Nodo2(20))
+
+#Ejemplo 3
+b = Nodo2(10 , Nodo2(20, Nodo2(30)))
+
+#Ejemplo 4
+b = Nodo2(10 , Nodo2(20, Nodo2(30 , Nodo2(40 , Nodo2(50)))))
+
+#Recorrido Transversal b
+curr_node = b
 print(curr_node.dato , "---> " , end = "")
 while(curr_node.siguiente != None):
     curr_node = curr_node.siguiente
