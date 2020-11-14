4459f5c8bdfc1d468f1181db0667008c8dd26763
Mik3Mon <mmonzonjr@gmail.com>
2020-11-13 19:45:32 -0600

    Juego de la vida v1.0.3

--- Tarea_5/main.py
+++ Tarea_5/main.py
@@ -1,23 +1,35 @@
 from generacion_y_reglas import Generacion
 
-print("\n>Partida de ejemplo Excel<")
 ejemplo = Generacion(10, 10, 6, [(1, 2), (2, 1), (2, 2), (2, 3)])
+
+print("\n>Partida de ejemplo Excel<")
 print("\nGeneracion Inical")
+
 ejemplo.imprime_grid()
 
 for gen in range(ejemplo.generaciones()):
+
     sig_generacion = gen + 1
+
     ejemplo.reglas_evolutivas()
+
     print(f"\nGeneracion {sig_generacion}")
+
     ejemplo.imprime_grid()
 
-print("\n>Partida aleatoria<")
 aleatorio = Generacion(15, 15, 10, [(4, 7), (5, 6), (5, 5), (4, 6), (1, 2), (2, 1), (2, 2), (2, 3)])
+
+print("\n>Partida aleatoria<")
 print("\nGeneracion Inical")
+
 aleatorio.imprime_grid()
 
 for gen in range(aleatorio.generaciones()):
+
     sig_generacion = gen + 1
+
     aleatorio.reglas_evolutivas()
+
     print(f"\nGeneracion {sig_generacion}")
+
     aleatorio.imprime_grid()
