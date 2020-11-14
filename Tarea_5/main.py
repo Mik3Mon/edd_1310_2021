bdf7fedb3b48bc38c59225df4238afb72eb78ccb
Mik3Mon <mmonzonjr@gmail.com>
2020-11-13 19:52:38 -0600

    Juego de la vida v1.0.3

--- Tarea_5/main.py
+++ Tarea_5/main.py
@@ -8,13 +8,9 @@ print("\nGeneracion Inical")
 ejemplo.imprime_grid()
 
 for gen in range(ejemplo.generaciones()):
-
     sig_generacion = gen + 1
-
     ejemplo.reglas_evolutivas()
-
     print(f"\nGeneracion {sig_generacion}")
-
     ejemplo.imprime_grid()
 
 aleatorio = Generacion(15, 15, 10, [(4, 7), (5, 6), (5, 5), (4, 6), (1, 2), (2, 1), (2, 2), (2, 3)])
@@ -25,11 +21,7 @@ print("\nGeneracion Inical")
 aleatorio.imprime_grid()
 
 for gen in range(aleatorio.generaciones()):
-
     sig_generacion = gen + 1
-
     aleatorio.reglas_evolutivas()
-
     print(f"\nGeneracion {sig_generacion}")
-
     aleatorio.imprime_grid()
