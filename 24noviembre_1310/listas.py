9bbb7f2d2b36c387b334ce0b581aa20b494bbf8e
Mik3Mon <mmonzonjr@gmail.com>
2020-11-24 11:45:06 -0600

    24noviembre_1310

--- 24noviembre_1310/listas.py
+++ 24noviembre_1310/listas.py
@@ -6,6 +6,22 @@ class NodoDoble:
 
 class DoubleLinkedList:
     def __init__(self):
-        self.__head = None
-        self.__tail = None
+        self.__head = NodoDoble(None)
+        self.__tail = NodoDoble(None)
         self.__size = 0
+
+    def get_size(self):
+        return self.__size
+
+    def is_empty(self):
+        return self.__size == 0
+
+    def append(self , value):
+        if self.is_empty():
+            nuevo = NodoDoble(value)
+            self.__head = nuevo
+            self.__tail = nuevo
+        else:
+            nuevo = NodoDoble(value , self.__tail , None)
+            self.__tail.next = nuevo
+            self.__tail = nuevo
