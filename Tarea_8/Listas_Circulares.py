3dc752ffa4dfd0c625394b6bbedfd90e21fd261a
Mik3Mon <mmonzonjr@gmail.com>
2020-12-01 02:34:33 -0600

    Tarea_8 v1.0.1

--- Tarea_8/Listas_Circulares.py
+++ Tarea_8/Listas_Circulares.py
@@ -13,7 +13,7 @@ class CircularList:
         return self.__head == None
 
     def get_size(self):
-        return print(f"\nSize ==> {self.__size}\n")
+        return print(f"Tamanio de la Lista Circular --> {self.__size}\n")
 
     def search(self , value = None):
         valor = False
@@ -31,10 +31,10 @@ class CircularList:
         if self.is_empty():
             self.__head = self.__referencia = nueva
             self.__referencia.siguiente = self.__head
-            print(f" Se agrego el elemento {value} a la Lista Circular\n")
+            print(f" >Se agrego el elemento {value}<\n")
             self.__size += 1
         elif self.search(value) == True:
-            print(f" El elemento {value} ya existe en la Lista Circular\n")
+            print(f" >Se intento agregar el elemento {value} pero este ya existe<\n")
         else:
             while self.search(value) == False:
                 if value > self.__referencia.data:
@@ -55,15 +55,36 @@ class CircularList:
                     nueva.siguiente = curr_prev.siguiente
                     curr_prev.siguiente = nueva
                     self.__size += 1
-            print(f" Se agrego el elemento {value} a la Lista Circular\n")
+            print(f" >Se agrego el elemento {value}<\n")
 
     def transversal(self):
         curr_node = self.__referencia
         while curr_node.siguiente != self.__referencia:
             print(f"{curr_node.siguiente.data} --> " , end = "")
             curr_node = curr_node.siguiente
-        print(f"{curr_node.siguiente.data} --> {self.__head.data}")
+        print(f"{curr_node.siguiente.data} --> {self.__head.data}\n")
 
     def remove(self , value = None):
-        if self.is_empty():
-            print("Lista Circular vacia")
+        curr_node = self.__referencia
+        if value != curr_node.data:
+            print(f" >El elemento {value} no existe<\n")
+        else:
+            curr_ref = self.__referencia
+            while curr_ref.data != value and curr_ref.siguiente != self.__referencia:
+                curr_prev = curr_ref
+                curr_ref = curr_ref.siguiente
+            print(f" >Se elimino el elemento {curr_ref.data}<\n")
+            if curr_ref.data == self.__head.data:
+                self.__head = self.__head.siguiente
+                self.__referencia.siguiente = self.__head
+                self.__size -= 1
+            elif curr_ref.data == self.__referencia.data:
+                curr_ref = self.__head
+                while curr_ref.siguiente != self.__referencia:
+                    curr_ref = curr_ref.siguiente
+                curr_ref.siguiente = self.__head
+                self.__referencia = curr_ref
+                self.__size -= 1
+            else:
+                curr_prev.siguiente = curr_prev.siguiente.siguiente
+                self.__size -= 1
